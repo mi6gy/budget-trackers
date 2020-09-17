b92c98e99228c6579daf0abc50bd3881b5cbe0ab
Miguel Jimenez <mjimenez08@gmail.com>
2020-09-17 15:23:10 -0400

    mongo

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,23 +2,41 @@ var URL_CACHE = [
     '/',
     '/db.js',
     '/index.js',
+    'index.html',
     '/styles.css',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
-    '/manifest.json'
+    '/manifest.webmanifests'
 ]
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 self.addEventListener("install", function(evt) {
     evt.waitUntil(
-      caches.open(CACHE_NAME).then(cache => {
+      caches.open(CACHE_NAME).then((cache) => {
         console.log("Your files were pre-cached successfully!");
         return cache.addAll(URL_CACHE);
       })
     );
   
-    // self.skipWaiting();
+    self.skipWaiting();
+  });
+
+  self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+      caches.keys().then((keyList) => {
+        return Promise.all(
+          keyList.map((key) => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
   });
 
 self.addEventListener("fetch", function(evt) {
